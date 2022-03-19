aaf8a8dd9b2d9b9563a216f9b6b3787d2b79d359
Arni Magnusson <thisisarni@gmail.com>
2022-03-20 01:29:16 +1100

    Development version of bevholt()

---
+++ R/bevholt.R
@@ -0,0 +1,21 @@
+#' Beverton-Holt
+#'
+#' Fit a Beverton-Holt recruitment model.
+#'
+#' @param S spawning biomass.
+#' @param R recruitment.
+#' @param par initial parameter values.
+#'
+#' @return Vector of estimated parameter values.
+#'
+#' @examples
+#' bevholt()
+#'
+#' @export
+
+bevholt <- function(S, R, par=list(logRmax=0, logS50=0, logSigma=0))
+{
+  model <- list(env=list(last.par.best=par))
+  best <- model$env$last.par.best
+  best
+}

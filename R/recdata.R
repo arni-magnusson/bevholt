e81c0751bacb469a504f8b6a5144b2db95c265ac
Arni Magnusson <thisisarni@gmail.com>
2022-03-21 00:14:46 +1100

    Add recdata

---
+++ R/recdata.R
@@ -0,0 +1,24 @@
+#' @docType data
+#'
+#' @name recdata
+#'
+#' @title Recruitment Data
+#'
+#' @description
+#' Simulated stock-recruitment dataset.
+#'
+#' @usage
+#' recdata
+#'
+#' @format
+#' Data frame containing two columns:
+#' \tabular{ll}{
+#'   \code{S} \tab spawning biomass\cr
+#'   \code{R} \tab recruitment
+#' }
+#'
+#'
+#' @examples
+#' recdata
+
+NA

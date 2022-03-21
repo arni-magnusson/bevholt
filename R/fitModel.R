e1ff19e89c92ea6be8a24eed09cdf7a3b119c2cc
Arni Magnusson <thisisarni@gmail.com>
2022-03-21 16:42:55 +1100

    Return model, fit, and report

--- R/fitModel.R
+++ R/fitModel.R
@@ -7,11 +7,14 @@
 #' @param parameters vector of initial parameter values.
 #' @param quiet whether to suppress gradient messages.
 #'
-#' @return List containing (TMB) \code{model} and (\code{nlminb}) \code{fit}.
+#' @return
+#' List containing (TMB) \code{model}, (nlminb) \code{fit}, and (SD)
+#' \code{report}.
 #'
 #' @note
 #' The formulation used is
-#' \eqn{R = R_\mathrm{max} \frac{S}{S+S_{50}}}{R = Rmax * S / (S + S50)}.
+#' \eqn{\hat{R} = R_\mathrm{max} \frac{S}{S+S_{50}}}
+#' {Rhat = Rmax * S / (S + S50)}.
 #'
 #' \eqn{R_\mathrm{max}}{Rmax} is maximum recruitment and \eqn{S_{50}}{S50} is
 #' the stock size that produces \eqn{0.50R_\mathrm{max}}{0.50 Rmax}.
@@ -20,15 +23,13 @@
 #' fm <- fitModel(recdata)
 #' fm$fit$par
 #' fm$fit$objective
-#' report <- sdreport(fm$model)
-#' report
-#' Rhat <- as.data.frame(summary(report)[rownames(summary(report))=="Rhat",])
-#' data.frame(data, Rhat=Rhat$Estimate)
+#' Rhat <- summary(fm$report)[rownames(summary(fm$report))=="Rhat",]
+#' data.frame(recdata, Rhat=Rhat[,"Estimate"])
 #'
 #' @useDynLib bevholt
 #'
 #' @importFrom stats nlminb
-#' @importFrom TMB MakeADFun
+#' @importFrom TMB MakeADFun sdreport
 #'
 #' @export
 
@@ -37,5 +38,6 @@ fitModel <- function(data, parameters=list(logRmax=0, logS50=0, logSigma=0),
 {
   model <- MakeADFun(data, parameters, DLL="bevholt", silent=quiet)
   fit <- nlminb(model$par, model$fn, model$gr)
-  list(model=model, fit=fit)
+  report <- sdreport(model)
+  list(model=model, fit=fit, report=report)
 }
